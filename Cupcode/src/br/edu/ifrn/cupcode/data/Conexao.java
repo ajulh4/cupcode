99d488c64eebe3db8d6b8a686a01f853893cd892
Júlia Ayres <katllynjcosta016@gmail.com>
2022-08-17 09:25:18 -0300

    fifth commit - criação do pacote das classes de conexão

---
+++ Cupcode/src/br/edu/ifrn/cupcode/data/Conexao.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.edu.ifrn.cupcode.data;
+
+/**
+ * @author cupCode[]
+ *
+ */
+public class Conexao {
+
+	/**
+	 * Aqui vai vim os códigos que Daniel vai mandar depois
+	 * 
+	 */
+}

08786e91733e2153324130eff05345640839fb2f
20201114010003 <20201114010003@SC445668.ifrn.local>
2022-08-23 17:04:52 -0300

    criação do dificuldadeDAO - vini

--- Cupcode/src/br/edu/ifrn/cupcode/data/Conexao.java
+++ Cupcode/src/br/edu/ifrn/cupcode/data/Conexao.java
@@ -1,16 +1,45 @@
-/**
- * 
- */
 package br.edu.ifrn.cupcode.data;
 
-/**
- * @author cupCode[]
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 
-	/**
-	 * Aqui vai vim os códigos que Daniel vai mandar depois
-	 * 
-	 */
+	private static final String URL = "jdbc:mysql://localhost:3306/cupcode_db?useTimezone=true&serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String SENHA = "root";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; 
+	private static Connection connection;
+	
+	public static Connection conectar(){
+		
+		try { 
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, USER, SENHA);
+			return connection;
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Erro aqui: " + e1.getMessage());
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+		
+	}
+			
+	public static void desconectar(){
+		
+		try{
+			if(connection != null){
+				connection.close();
+				connection = null;
+			}
+		}
+		
+		catch(SQLException ex){
+			System.out.println(ex.getMessage());
+			
+		}
+		
+	}
 }
